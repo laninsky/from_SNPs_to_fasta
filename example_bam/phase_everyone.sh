78eb859eea877dbe91af3c7f4e868cfc26333415
Alana Alexander <laninsky@gmail.com>
2018-03-27 12:00:38 +1300

    Update phase_everyone.sh

--- example_bam/phase_everyone.sh
+++ example_bam/phase_everyone.sh
@@ -33,8 +33,7 @@ samtools index tempsortmarked.bam;
 #$gatk/gatk -T IndelRealigner -R contigs_to_probes.fasta -I  tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam;
 # The -stand_emit_conf 30 option is deprecated in GATK v 3.7 and was removed from this code on the 5-June-2017
 $gatk/gatk HaplotypeCaller -R contigs_to_probes.fasta -I tempsortmarked.bam-stand-call-conf 30 -O temp_raw_variants.vcf;
-$gatk/gatk ReadBackedPhasing -R contigs_to_probes.fasta -I tempsortmarked.bam --variant temp_raw_variants.vcf -O temp_phased_SNPs.vcf;
-$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R contigs_to_probes.fasta -O temp_alt.fa;
+$gatk/gatk FastaAlternateReferenceMaker -V temp_raw_variants.vcf  -R contigs_to_probes.fasta -O temp_alt.fa;
 
 Rscript onelining.R;
 
@@ -66,8 +65,7 @@ samtools index tempsortmarked.bam;
 #$gatk/gatk -T IndelRealigner -R $name.fa -I  tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam;
 # The -stand_emit_conf 30 option is deprecated in GATK v 3.7 and was removed from this code on the 5-June-2017
 $gatk/gatk HaplotypeCaller -R $name.fa -I tempsortmarked.bam -stand-call-conf 30 -O temp_raw_variants.vcf;
-$gatk/gatk ReadBackedPhasing -R $name.fa -I tempsortmarked.bam  --variant temp_raw_variants.vcf -O temp_phased_SNPs.vcf;
-$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R $name.fa -O temp_alt.fa;
+$gatk/gatk FastaAlternateReferenceMaker -V temp_raw_variants.vcf -R $name.fa -O temp_alt.fa;
 
 Rscript onelining.R;
 
