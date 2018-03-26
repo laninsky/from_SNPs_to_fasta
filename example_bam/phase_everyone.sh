177ce15412d44027e5b90411956e8ff0d644227c
Alana Alexander <laninsky@gmail.com>
2018-03-27 11:41:40 +1300

    Update phase_everyone.sh

--- example_bam/phase_everyone.sh
+++ example_bam/phase_everyone.sh
@@ -32,9 +32,9 @@ samtools index tempsortmarked.bam;
 #$gatk/gatk -T RealignerTargetCreator -R contigs_to_probes.fasta -I tempsortmarked.bam -o tempintervals.list;
 #$gatk/gatk -T IndelRealigner -R contigs_to_probes.fasta -I  tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam;
 # The -stand_emit_conf 30 option is deprecated in GATK v 3.7 and was removed from this code on the 5-June-2017
-$gatk/gatk HaplotypeCaller -R contigs_to_probes.fasta -I temp_realigned_reads.bam -stand-call-conf 30 -o temp_raw_variants.vcf;
-$gatk/gatk ReadBackedPhasing -R contigs_to_probes.fasta -I temp_realigned_reads.bam  --variant temp_raw_variants.vcf -o temp_phased_SNPs.vcf;
-$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R contigs_to_probes.fasta -o temp_alt.fa;
+$gatk/gatk HaplotypeCaller -R contigs_to_probes.fasta -I temp_realigned_reads.bam -stand-call-conf 30 -O temp_raw_variants.vcf;
+$gatk/gatk ReadBackedPhasing -R contigs_to_probes.fasta -I temp_realigned_reads.bam  --variant temp_raw_variants.vcf -O temp_phased_SNPs.vcf;
+$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R contigs_to_probes.fasta -O temp_alt.fa;
 
 Rscript onelining.R;
 
@@ -65,9 +65,9 @@ samtools index tempsortmarked.bam;
 #$gatk/gatk -T RealignerTargetCreator -R $name.fa -I tempsortmarked.bam -o tempintervals.list;
 #$gatk/gatk -T IndelRealigner -R $name.fa -I  tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam;
 # The -stand_emit_conf 30 option is deprecated in GATK v 3.7 and was removed from this code on the 5-June-2017
-$gatk/gatk HaplotypeCaller -R $name.fa -I temp_realigned_reads.bam -stand-call-conf 30 -o temp_raw_variants.vcf;
-$gatk/gatk ReadBackedPhasing -R $name.fa -I temp_realigned_reads.bam  --variant temp_raw_variants.vcf -o temp_phased_SNPs.vcf;
-$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R $name.fa -o temp_alt.fa;
+$gatk/gatk HaplotypeCaller -R $name.fa -I temp_realigned_reads.bam -stand-call-conf 30 -O temp_raw_variants.vcf;
+$gatk/gatk ReadBackedPhasing -R $name.fa -I temp_realigned_reads.bam  --variant temp_raw_variants.vcf -O temp_phased_SNPs.vcf;
+$gatk/gatk FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R $name.fa -O temp_alt.fa;
 
 Rscript onelining.R;
 
